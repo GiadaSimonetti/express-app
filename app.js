d03c1dfbffbc7e0b04eb5fdb817d1860ff95f1e7
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-06-01 12:04:28 +0100

    Correct error

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
 app.get('/', (req, res) =>
-  res.send('Hello f*****g World!'))
+  res.send('Hello f*****g World!'));
 
 app.get('/hello', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
 app.get('/tdd', (req, res) => {
-  res.render('tdd')
-})
+  res.render('tdd');
+});
 
-app.set('view engine', 'ejs');
-app.set('views', 'views')
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-module.exports = app
+module.exports = app;

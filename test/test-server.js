2e794b0c977c34249c30f4dc95932757f1681866
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-06-01 10:09:09 +0100

    Add 2 more tests

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../server/app');
+var server = require('../app');
 var should = chai.should();
 
 chai.use(chaiHttp);
@@ -12,6 +12,26 @@ describe('Hello world', () => {
     .get('/')
     .end((req, res) => {
       res.text.should.contain('Hello f*****g World!');
+    });
+  });
+});
+
+describe('Hello', () => {
+  it('checks if page hello has status of 200', () => {
+    chai.request(server)
+    .get('/hello')
+    .end((req, res) => {
+      res.should.have.status(200);
+    });
+  });
+});
+
+describe('tdd', () => {
+  it('checks if `TDD!!!!!` is in the page', () => {
+    chai.request(server)
+    .get('/tdd')
+    .end((req, res) => {
+      res.text.should.contain('TDD!!!!!!');
     });
   });
 });
